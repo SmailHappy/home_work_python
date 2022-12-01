6ed599c9cef737d4da1ec74006e67b27850f615d
Alexey <alex-ey98@mail.ru>
2022-12-01 19:24:11 +0300

    Домашнее задание 3

---
+++ dz_3.py
@@ -0,0 +1,162 @@
+from random import randint
+from random import random
+import os
+from math import ceil
+
+def Run() :
+    code = int(input ("Задача 11 - 11. Задача 12 - 12. Задача 13 - 13. Задача 14 - 14. Задача 15 - 15.\nВведите код задачи: "))
+    if code == 11 :
+        zadacha_11()
+    elif code == 12 :
+        zadacha_12()
+    elif code == 13 :
+        zadacha_13()
+    elif code == 14 :
+        zadacha_14()
+    elif code == 15 :
+        zadacha_15()
+    else :
+        print ("Такой задачи тут нет.")
+
+def zadacha_11() :
+    # Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+    # Пример: - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+
+    os.system('CLS')
+
+    print("Задача 11. Создаёт список из N элементов и выводит сумму нечётных элементов\n")
+    n = int(input("Введите N: "))
+
+    fill_numbers = [randint(0, 10) for i in range(n)]
+    sum = 0
+    print (f"Список - {fill_numbers}") 
+
+    # for i in range(1, n) :
+    #     if i % 2 == 1 :
+    #         sum += fill_numbers[i]
+
+    i = 1
+    while i < n :
+        sum += fill_numbers[i]
+        i += 2
+
+    print (f"Сумма цифр на нечётных позициях = {sum}") 
+
+    Break()
+
+def zadacha_12() :
+    # Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+    # Пример:
+    # - [2, 3, 4, 5, 6] => [12, 15, 16];
+    # - [2, 3, 5, 6] => [12, 15]
+
+    os.system('CLS')
+
+    print("Задача 12. Создаёт список из N элементов и найдёт произведение пар чисел списка ( пара - первый и последний элемент )\n")
+    n = int(input("Введите N: "))
+
+    fill_numbers = [randint(0, 10) for i in range(n)]
+    print (f"Список - {fill_numbers}")
+
+    # if n % 2 == 1 :
+    #     index = n // 2 + 1
+    # else :
+    #     index = n // 2
+
+    multi_pairs = [fill_numbers[i] * fill_numbers[n - 1- i] for i in range(ceil(n / 2))] # ceil(n / 2) == index
+    print (f"Список умноженных пар - {multi_pairs}")
+
+    Break()
+
+def zadacha_13() :
+    # Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
+    # Пример: - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+    os.system('CLS')
+
+    print("Задача 13. Создаёт список из N элементов вещественных чисел и находит разницу между макс. и мин. дробной части элементов\n")
+    n = int(input("Введите N: "))
+
+    fill_numbers = [round(random() + randint(0, 10), 2) for i in range(n)]
+    print (f"Список - {fill_numbers}")
+
+    max = fill_numbers[0] - fill_numbers[0] // 1
+    min = fill_numbers[0] - fill_numbers[0] // 1
+
+    for i in fill_numbers :
+        j = i - i // 1
+        if j > max :
+            max = j
+        elif j < min :
+            min = j 
+
+    diff = max - min
+    print(f"Разница = {round(diff, 2)}")
+
+    Break()
+
+def zadacha_14() :
+    # Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+    # Пример:
+    # - 45 -> 101101
+    # - 3 -> 11
+    # - 2 -> 10
+
+    os.system('CLS')
+
+    print("Задача 14. Преобразовывать десятичное число N в двоичное\n")
+    n = int(input("Введите N: "))
+
+    result = ''
+
+    while n != 1:
+        if n % 2 == 1:
+            result = '1' + result
+            n = (n - 1) / 2
+        elif n % 2 == 0:
+            result = '0' + result
+            n = n / 2
+            
+    if n == 0:
+        result = '0' + result
+    elif n == 1:
+        result = '1' + result
+
+    print(f"В двоичной системе: {result}")
+
+    Break()
+
+def zadacha_15() :
+    # Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+    # Пример:
+    # - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+    os.system('CLS')
+
+    print("Задача 15. Составляет список чисел Фибоначчи, в том числе для отрицательных индексов\n")
+    n = int(input("Введите N: "))
+
+    def fib(number, mode = 0) :
+        if number == 1 or number == 0 :
+            return 1
+        else :
+            if mode == 1:
+                return fib (number - 2, 1) - fib (number - 1, 1)
+            else :
+                return fib (number - 1) + fib (number - 2)
+
+    list = [0]
+    for i in range(n) :
+        list.insert(0, fib(i + 3, 1))
+        list.append(fib(i))
+
+    print(list)
+
+    Break()
+
+def Break() :
+    code = int(input("\nДля выбора другой задачи - 5. Для завершения - 0\n"))
+    if code == 5 :
+        Run()
+
+Run()
