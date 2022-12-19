4fc7ecea82cb4f82f894842804de468997d51ae0
Alexey <alex-ey98@mail.ru>
2022-12-19 16:55:30 +0300

    дз 7

--- dz_7/model.py
+++ dz_7/model.py
@@ -1,14 +1,14 @@
 def export():
     format_knigi = input('\t1. CSV\n\t2. TXT\nС каким форматом хотите работать? ')
     if format_knigi == '1':
-        with open('phone_book.csv', 'a', encoding='utf-8') as file:
+        with open('home_work/dz_7/phone_book.csv', 'a', encoding='utf-8') as file:
             name = input('\nВведите имя: ')
             name_2 = input('Введите фамилию: ')
             telefon_number = input('Введите номер телефона: ')
             data = name_2 + ',' + name + ',' + telefon_number
             file.write('\n' + data)
     else:
-        with open('phone_book.txt', 'a', encoding='utf-8') as file:
+        with open('home_work/dz_7/phone_book.txt', 'a', encoding='utf-8') as file:
             name = input('Введите имя: ')
             name_2 = input('Введите фамилию: ')
             telefon_number = input('Введите номер телефона: ')
@@ -19,14 +19,14 @@ def import_cont():
     format_knigi = input('\t1. CSV\n\t2. TXT\nС каким форматом хотите работать? ')
     if format_knigi == '1':
         print("\nФамилия\t\tИмя\t\tНомер Телефона\n")
-        with open('phone_book.csv', 'r', encoding='utf-8') as file:
+        with open('home_work/dz_7/phone_book.csv', 'r', encoding='utf-8') as file:
             for line in file:
                 import_cont = line.split(',')
                 print('\t\t'.join(import_cont),end='')
         print('\n')
     else:
         print("\nФамилия\t\tИмя\t\tНомер Телефона\n")
-        with open('phone_book.txt', 'r', encoding='utf-8') as file:
+        with open('home_work/dz_7/phone_book.txt', 'r', encoding='utf-8') as file:
             for line in file:
                 import_cont = line.split(',')
                 print('\t\t'.join(import_cont),end='')
@@ -38,7 +38,7 @@ def search_cont():
     if format_knigi == '1':
         count = 0
         name_2 = input('\nВведите фамилию или имя абонента: ')
-        with open('phone_book.csv', 'r', encoding='utf-8') as file:
+        with open('home_work/dz_7/phone_book.csv', 'r', encoding='utf-8') as file:
             for line in file:
                 if name_2 in line:
                     print("\nФамилия\t\tИмя\t\tНомер Телефона\n")
@@ -48,7 +48,7 @@ def search_cont():
     else:
         count = 0
         name_2 = input('\nВведите фамилию или имя абонента: ')
-        with open('phone_book.txt', 'r', encoding='utf-8') as file:
+        with open('home_work/dz_7/phone_book.txt', 'r', encoding='utf-8') as file:
             for line in file:
                 if name_2 in line:
                     print("\nФамилия\t\tИмя\t\tНомер Телефона\n")
